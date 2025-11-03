c9193fb3e1653840116e5997b91ecd537053fb54
Wrivard <wrivard@kua.quebec>
2025-11-03 14:48:36 -0500

    Update FAB buttons: use Messenger icon SVG and normal arrow for back to top

--- js/fab-buttons.js
+++ js/fab-buttons.js
@@ -14,14 +14,11 @@
   // Button markup template
   const buttonMarkup = `
     <a href="${MESSENGER_URL}" target="_blank" class="messenger-button" aria-label="Contactez-nous sur Messenger">
-      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16 2C8.268 2 2 7.925 2 15.213c0 4.446 2.225 8.41 5.725 11.015l-1.5 5.362c-.093.339.238.63.561.468l5.725-3.004c1.519.425 3.131.656 4.824.656h.15c7.732 0 14-5.925 14-13.213S23.732 2 16 2z" fill="#ffffff"/>
-        <path d="M10.5 15.5c0-.828.672-1.5 1.5-1.5h8c.828 0 1.5.672 1.5 1.5s-.672 1.5-1.5 1.5h-8c-.828 0-1.5-.672-1.5-1.5z" fill="#72adcb"/>
-      </svg>
+      <img src="/images/Messenger_Icon_Secondary_White.svg" alt="Messenger" />
     </a>
     <button class="st_button" aria-label="Retour en haut">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 4L4 12h3v8h10v-8h3L12 4z" fill="currentColor"/>
+        <path d="M12 4l-8 8h6v8h4v-8h6l-8-8z" fill="currentColor"/>
       </svg>
     </button>
   `;
